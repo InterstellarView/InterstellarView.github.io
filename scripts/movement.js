52d47447eb17e083bee700585925d449a9abbcbc
InterstellarView <56245383+InterstellarView@users.noreply.github.com>
2019-10-12 13:11:53 -0600

    ah shit, here we go again

--- scripts/movement.js
+++ scripts/movement.js
@@ -1,1 +1,10 @@
 
+function myGameArea = {
+    canvas : document.createElement("canvas")
+    start : function() {
+        this.canvas.width = 5000;
+        this.canvas.height = 5000;
+        this.context = this.canvas.getContext ("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+|
