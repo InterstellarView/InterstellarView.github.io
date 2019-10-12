044be6348e8fb716cdf55ac2d39f3eaed5403468
InterstellarView <56245383+InterstellarView@users.noreply.github.com>
2019-10-12 14:31:05 -0600

    Update movement.js

--- scripts/movement.js
+++ scripts/movement.js
@@ -22,11 +22,23 @@ function startGame() {
     myGamePiece = new component(30, 30, "../images/Ship.png", 10, 120, "image");
 }
     //Defines objects or whatever
-function component(width, height, color, x, y) {
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    }
+    
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
+    if (type == "image") {
+        ctx.drawImage(this.image,
+          this.x,
+          this.y,
+          this.width, this.height);
+      } else {
     ctx = myGameArea.context;
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
