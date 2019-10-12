cd5f1b1e94b7b879a77d2e7c135e78d052df014e
InterstellarView <56245383+InterstellarView@users.noreply.github.com>
2019-10-12 14:02:52 -0600

    if this works ill be ecstatic

--- scripts/movement.js
+++ scripts/movement.js
@@ -1,7 +1,6 @@
-function startGame() {
-    myGameArea.start();
-}
+// Variables
 
+    //Stars And Mapping
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -10,4 +9,25 @@ var myGameArea = {
         this.context = this.canvas.getContext ("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
+}
+
+    //Ship
+var myGamePiece;
+
+// Functions
+
+    //Loads everything in
+function startGame() {
+    myGamePiece = new component(30, 30, "../images/Ship.png", 10, 120, "image");
+    myGameArea.start();
+}
+    //Defines objects or whatever
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = myGamearea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
 }
