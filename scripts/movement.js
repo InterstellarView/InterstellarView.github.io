3517943d9ab87fe1e524b8b921cbf938a8fc276b
poshpotato <56280387+poshpotato@users.noreply.github.com>
2019-10-13 09:30:45 +1300

    frame thing

--- scripts/movement.js
+++ scripts/movement.js
@@ -9,6 +9,9 @@ var myGameArea = {
         this.context = this.canvas.getContext ("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 }
 
     //Ship
