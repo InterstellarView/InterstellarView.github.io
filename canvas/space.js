e7fb3ea2e146118bd2bba0ff96aeb1caf9c47a5a
poshpotato <56280387+poshpotato@users.noreply.github.com>
2019-11-26 10:33:09 +1300

    codey-doo

--- canvas/space.js
+++ canvas/space.js
@@ -2,8 +2,23 @@ var canvas = document.getElementById("spaceCanvas");
 var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+class planet {
+    constructor(x, y, image, positionX, positionY,){
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+}
 var ship = document.getElementById("ship");
-var background = document.getElementById("background")
+var background = document.getElementById("background");
+var planetList = [];
+var krysalPlanetImage = document.getElementById("krysalPlanet");
+var backgroundX = 0;
+var backgroundY = 0;
+var backgroundWidth;
+var backgroundHeight;
 var shipWidth;
 var shipHeight;
 var shipX;
@@ -16,16 +31,19 @@ var downPressed = false;
 var leftPressed = false;
 var rightPressed = false;
 var loopBuffer = 10;
+
 function afterLoad() {
     // the stuff you want to do after page load happens here
-    background.width = canvas.width;
-    background.height = canvas.height;
     shipWidth = ship.width;
     shipHeight = ship.height;
+    backgroundWidth = background.width;
+    backgroundHeight = background.height;
     shipX = canvas.width/2-shipWidth/2;
     shipY = canvas.height/2-shipHeight/2;
-    console.log(shipX);
-    console.log(shipY);
+    backgroundX = canvas.width/2-backgroundWidth/2;
+    backgroundY = canvas.height/2-backgroundHeight/2;
+    const krysalPlanet = new planet(backgroundWidth*0.75, backgroundHeight*0.5, krysalPlanetImage, backgroundWidth*0.75, backgroundHeight*0.5);
+    planetList.push(krysalPlanet);
     setInterval(draw, 10);
 }
 
@@ -76,28 +94,36 @@ function keyUpHandler(e){
     }
 }
 
+
+function drawPlanets() {
+    for(var p=0; p < planetList.length; p++) {
+        planetList[p].x = ((shipX/2)*-1)+planetList[p].positionX;
+        planetList[p].y = ((shipY/2)*-1)+planetList[p].positionY;
+        ctx.drawImage(planetList[p].image, planetList[p].x, planetList[p].y);
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(background, 0, 0)
+    ctx.drawImage(background, backgroundX, backgroundY,)
+    drawPlanets()
     ctx.drawImage(ship, shipX, shipY);
     shipX += shipVelX;
     shipY += shipVelY;
+    backgroundX = (shipX/2)*-1;
+    backgroundY = (shipY/2)*-1;
     shipVelX *= shipSlowSpeed;
     shipVelY *= shipSlowSpeed;
     
     if(leftPressed == true){
-        console.log("Left");
         shipVelX -= 0.1;
     } else if(rightPressed == true) {
-        console.log("Right");
         shipVelX += 0.1;
     }
     
     if(upPressed == true) {
-        console.log("Up");
         shipVelY -= 0.1;
     } else if (downPressed == true) {
-        console.log("Down")
         shipVelY += 0.1;
     }
     
@@ -113,4 +139,3 @@ function draw() {
     }
    
 }
-
